eb6181b7936175d719ac56e829fcac2ea179572f
Camille <camille.morand-duval@student-cs.fr>
2020-05-08 09:54:59 +0100

    initial R file

---
+++ HMW1/homework1.R
@@ -0,0 +1,17 @@
+# Financial Econometrics - Homework 1 
+# 08.05.2020
+# Raphael Attali, Camille Morand-Duval, Niels Nicolas
+
+#### QUESTION 1 ####
+
+
+#### QUESTION 2 ####
+
+
+#### QUESTION 3 ####
+
+
+#### QUESTION 4 ####
+
+
+#### QUESTION 5 ####
