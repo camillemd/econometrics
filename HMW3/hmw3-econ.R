ecde31178bd35e0e951771d9f330906b8590e4c3
Camille <camille.morand-duval@student-cs.fr>
2020-05-31 18:39:28 +0100

    homework 3 - layout only

---
+++ HMW3/hmw3-econ.R
@@ -0,0 +1,38 @@
+# Financial Econometrics - Homework 1 
+# 31.05.2020
+# Raphael Attali, Camille Morand-Duval, Niels Nicolas, Debdeep Roy
+
+#### INITIALISATION ####
+
+# set dir
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+# libraries
+library(tidyquant)
+library(ggplot2)
+
+#### DATA ####
+
+
+#### QUESTION 1 ####
+
+
+#### QUESTION 2 ####
+
+
+#### QUESTION 3 ####
+
+
+#### QUESTION 4 ####
+
+
+#### QUESTION 5 ####
+
+
+#### QUESTION 6 ####
+
+
+#### QUESTION 7 ####
+
+
+#### QUESTION 8 ####
