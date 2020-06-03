4d359c86ae8179e093c115ee9a0b38de11f0b69e
Camille <camille.morand-duval@student-cs.fr>
2020-06-03 09:37:42 +0100

    question 1 continued

--- HMW3/hmw3-econ.R
+++ HMW3/hmw3-econ.R
@@ -11,11 +11,12 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 library(tidyquant)
 library(ggplot2)
 library(BatchGetSymbols)
+#library(broom)
 
 #### DATA ####
 
 start_date = '2010-01-01'
-end_date = '2020-04-01' #toString(Sys.Date()) #today
+end_date = '2020-05-01' # last date available in the FF factors file
 
 ## Stocks ##
 
@@ -24,8 +25,9 @@ companies <- GetSP500Stocks()
 stock <- NULL
 
 for (row in gsub('[.]', '-', companies$Tickers)){
-  stock_prices = getSymbols(row, from = start_date, to = end_date, src = "yahoo", 
-                            verbose = FALSE, auto.assign = FALSE)[,6]
+  stock_prices = getSymbols(row, from = start_date, to = end_date, 
+                            src = "yahoo", verbose = FALSE, 
+                            auto.assign = FALSE)[,6]
   stock = cbind(stock, stock_prices)
 }
 stock = data.frame(stock)
@@ -35,66 +37,55 @@ todayY <- as.numeric(format(Sys.Date(), "%Y"))
 todayM <- as.numeric(format(Sys.Date(), "%m"))
 
 temp <- tempfile()
-factors <- read.csv("F-F_Research_Data_Factors.CSV", sep = ",", 
-                    skip = 3, header = TRUE, nrows = 6+(todayY-1927)*12+todayM-2)
+factors <- read.csv("F-F_Research_Data_5_Factors_2x3.csv", sep = ",", 
+                    skip = 3, header = TRUE, 
+                    nrows = 7+(todayY-1963)*12+todayM-15)
 factors <- factors[factors$X > 200912,] # select only values after 2010
 
 # check NA
-sum(is.na(stock))
+sum(is.na(stock)) # some stocks have NA until 2020-03-19
+test_NA = stock[row.names(stock)>"2020-03-31",]
+sum(is.na(test_NA))
+stock[is.na(stock)] = 999
 
 # percentage log return
-ln_stock = log(stock) # log price
-diff_stock = diff(as.matrix(ln_stock))*100 # percentage log return
-#rows = dim(diff_stock)[1]
-
-#### USEFUL FUNCTIONS ####
-
-OLS <- function(y, X){
-  
-  T = dim(X)[1]
-  K = dim(X)[2]+1
-  X = cbind(rep(1,T),X)
-  b = solve(t(X)%*%X)%*%(t(X)%*%y)
-  res = y-X%*%b
-  
-  sig2 = 1/(T-K)* (t(res)%*%res)
-  var_b = diag(solve(t(X)%*%X)%*%diag(rep(sig2,K)))
-  
-  std_b = sqrt(var_b)
-  tstat = b/std_b
-  pval = 2 * (1-pnorm(abs(tstat)))
-  
-  R2 = 1 - (t(res)%*%res)/(t(y-mean(y))%*%(y-mean(y)))
-  print(c("R2",R2))
-  
-  rho_1 = 1/T*sum(res[2:T]*res[1:T-1])
-  print(c("rho_1(u)",rho_1))
-  
-  DW=sum((res[2:T]-res[1:T-1])^2)/sum(res*res)
-  print(c("DW",DW))
-  
-  z_DW=sqrt(T)*(DW/2 - 1)
-  print(c("z_DW",z_DW))
-  
-  pval_z = 2 * (1-pnorm(abs(z_DW)))
-  print(c("pval z_DW",pval_z))
-  
-  value <- cbind(b,std_b,tstat,pval, R2)
-  return(value)
-}
+log_stock = log(stock) # log price
+log_returns = diff(as.matrix(log_stock))*100 # percentage log return
 
 #### QUESTION 1 ####
 
 # aggregate values per months
-ri = aggregate(diff_stock, by=list(month(rownames(diff_stock)), year(rownames(diff_stock))), FUN=SUM)
-rownames(ri) = paste(ri$Group.2, ri$Group.1) # change index
+ri = aggregate(log_returns, by=list(month(rownames(log_returns)), 
+                                    year(rownames(log_returns))), FUN=SUM)
+ri = ri[,3:ncol(ri)]
 
-# calculate difference between total return and risk free rate of return
-ri_Rf = ri[,3:ncol(ri)] - factors$RF 
-
-ff = OLS(ri_Rf[,1],cbind(factors$Mkt.RF,factors$SMB,factors$HML))
+# calculate difference between total return and risk free rate of return 
+# for all stocks
+ri_RF = ri - factors$RF
 
 ## Farma-French Factors ##
+
+linear.regression <- function(ri_RF, factors, start_date, end_date){
+  
+  # create df to pass into linear regression
+  df = data.frame(ri_RF, factors$Mkt.RF, factors$SMB, factors$HML, 
+                  factors$RMW, factors$CMA)
+  df$date = seq(as.Date(start_date), as.Date(end_date) - 1, "months")
+  colnames(df)[colnames(df) == 'ri_Rf...1.'] = 'ri_RF'
+  colnames(df)[colnames(df) == 'factors.Mkt.RF'] = 'Mkt.RF'
+  colnames(df)[colnames(df) == 'factors.HML'] = 'HML'
+  colnames(df)[colnames(df) == 'factors.RMW'] = 'RMW'
+  colnames(df)[colnames(df) == 'factors.SMB'] = 'SMBL'
+  colnames(df)[colnames(df) == 'factors.CMA'] = 'CMA'
+  
+  ff = lm(ri_RF ~ Mkt.RF + HML + RMW + SMBL + CMA, data = df)
+  
+  
+  
+  
+}
+
+
 
 
 
